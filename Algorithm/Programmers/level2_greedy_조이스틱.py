ebfc3afdeb55b94b2e7ab440dc0dbd967087fe1c
wowchois <61316086+wowchois@users.noreply.github.com>
2022-04-05 00:06:49 +0900

    Update level2_greedy_조이스틱.py

--- Algorithm/Programmers/level2_greedy_조이스틱.py
+++ Algorithm/Programmers/level2_greedy_조이스틱.py
@@ -1,1 +1,11 @@
 
+
+def solution(name):
+    answer = 0
+    if set(name) == {'A'} : return 0
+    
+    for (idx,val) in enumerate(name) :
+        answer += min(ord(val) - ord('A'), ord('Z') - ord(val))
+    
+    
+    return answer
