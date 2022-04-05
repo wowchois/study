92d4a79327a61a8c15274a656ca69fb594d5b537
wowchois <61316086+wowchois@users.noreply.github.com>
2022-04-06 00:22:55 +0900

    Update level2_greedy_큰수만들기.py

--- Algorithm/Programmers/level2_greedy_큰수만들기.py
+++ Algorithm/Programmers/level2_greedy_큰수만들기.py
@@ -1,1 +1,21 @@
 
+def solution(number, k):
+    answer = ''
+    length = len(number)
+    loop = length - k
+    delete = 0
+    nIdx = 0
+
+    while delete != k :
+        for i in range(len(number)) :
+            if i+1 == len(number) : break
+            if number[i] < number[i+1] :
+                number = number.replace(number[i],'',1)
+                delete += 1
+                break
+            else :
+                nIdx += 1
+        if nIdx >= loop and len(number) == length :
+            return number[:loop]
+    
+    return number
