1eed29d5fb2d63c770a5a6a6d27519efd995221f
wowchois <61316086+wowchois@users.noreply.github.com>
2022-04-17 00:30:05 +0900

    Update kakao_2020_level2_문자열압축.py

--- Algorithm/Programmers/kakao_2020_level2_문자열압축.py
+++ Algorithm/Programmers/kakao_2020_level2_문자열압축.py
@@ -1,1 +1,39 @@
+'''
+TEST CASE
 
+s / result
+"aabbaccc"	7
+"ababcdcdababcdcd"	9
+"abcabcdede"	8
+"abcabcabcabcdededededede"	14
+"xababcdcdababcdcd"	17
+'''
+
+
+
+def solution(s):
+    answer = 0
+    loopLen = len(s)//2
+    resultArr = []
+    
+    if loopLen < 1 : #문자열 길이 1인 경우
+        return len(s)
+    
+    for i in range(1,loopLen+1) :
+        n = 1
+        result = ''
+        for j in range(0,len(s),i) :
+            #print(i,j,s[j:j+i],s[j+i:j+i+i])
+            if s[j:j+i] == s[j+i:j+i+i] :
+                n += 1
+            else :
+                if n > 1 :
+                    result += str(n)
+                n = 1
+                result += s[j:j+i]
+        resultArr.append(len(result))
+            
+    #print(resultArr)
+    answer = min(resultArr)
+    
+    return answer
