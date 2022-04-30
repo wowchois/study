3cd63ca41395550aa27054a64941d7ead65c047c
wowchois <61316086+wowchois@users.noreply.github.com>
2022-05-01 00:34:10 +0900

    Update level2_더맵게.py

--- Algorithm/Programmers/level2_더맵게.py
+++ Algorithm/Programmers/level2_더맵게.py
@@ -6,7 +6,8 @@ scoville	K	return
 [4] 5 -1
 '''
 
-# 정확도 100 효율성 0
+# 정확도 100 효율성 0 
+# 원인 : while문에서 min(scoville) 으로 비교했기 때문,,
 import heapq
 
 def solution(scoville, K):
@@ -25,5 +26,30 @@ def solution(scoville, K):
         answer += 1
     else:
         if len(scoville) == 1: return 1
+    
+    return answer
+
+
+
+# 효율성 통과한 경우 1
+while min(scoville) < K:    # before
+while scoville[0] < K:      # after
+
+
+# 효율성 통과한 경우 2 - 정확도 떨어짐 ([6] 5 1 케이스 통과못함)
+import heapq
+
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
+  
+    while scoville[0] < K:
+        try:
+            first_low = heapq.heappop(scoville)
+            second_low = heapq.heappop(scoville)
+            heapq.heappush(scoville, first_low + (second_low*2))
+        except:
+            return -1
+        answer += 1
     
     return answer
