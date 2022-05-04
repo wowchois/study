cbff212ffa94ce8e6882599dbaea8840a765c138
wowchois <61316086+wowchois@users.noreply.github.com>
2022-05-05 00:13:26 +0900

    Update level2_배상비용최소화.py

--- Algorithm/Programmers/level2_배상비용최소화.py
+++ Algorithm/Programmers/level2_배상비용최소화.py
@@ -1,13 +1,38 @@
 '''
+heap 문제
+
 TEST CASE
 
 N	works	result
 4	[4,3,3]	12
 2	[3,3,3]	17
 15 [4,4,4] 0
+
+1. 일할 시간이(no) 총 일의 양보다 크면 배상비용은 0
+2. 최대힙으로 처리하기 위해서 (-)처리
+3. heapreplace = pop -> push 순으로 실행
 '''
 
-# 효율성/정확성 통과
+# heap 사용
+import heapq
+
+def solution(no, works):
+    if no > sum(works): return 0
+
+    result = 0
+    works = [-x for x in works]
+    heapq.heapify(works)
+
+    for _ in range(no):
+        heapq.heapreplace(works,works[0]+1)
+    
+    result = sum([x**2 for x in works])
+
+    return result
+
+
+
+# sort 사용 - 효율성/정확성 통과
 def solution(no, works):
     result = 0
     idx = 0
@@ -28,7 +53,7 @@ def solution(no, works):
     return result
 
 
-# 코드 개선
+# sort사용 - 코드 개선
 def solution(no, works):
     result = 0
     idx = 0
