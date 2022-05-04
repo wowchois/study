70b0f1ca951a60540674a0914a3b4ae871d6d0a5
wowchois <61316086+wowchois@users.noreply.github.com>
2022-05-04 23:45:49 +0900

    Update level2_기능개발.py

--- Algorithm/Programmers/level2_기능개발.py
+++ Algorithm/Programmers/level2_기능개발.py
@@ -1,9 +1,15 @@
 '''
+Queue 문제
+
 TEST CASE
 
 progresses	speeds	return
 [93,30,55]	[1,30,5]	[2,1]
 
+1. 100-진행사항 = 남은작업 / speeds 한 결과를 올림처리한다.
+2. 첫번째 값을 max로 두고 비교하면서 처리.
+앞의 최대 작업시간보다 크면 따로 배포해야되서 answer에 추가,
+앞의 최대 작업보다 작거나 같으면 같이 배포해야되서 +1
 '''
 
 
@@ -13,7 +19,7 @@ import math
 
 def solution(progresses, speeds):
     answer = []
-    day = [math.ceil((100-p)/s) for p,s in zip(progresses,speeds)]
+    day = [math.ceil((100-p)/s) for p, s in zip(progresses, speeds)]
     que = deque(day)
     cnt = 1
     max_d = que.popleft()
@@ -36,16 +42,15 @@ import math
 def solution(progresses, speeds):
     answer = []
     max_d = math.ceil((100 - progresses[0])/speeds[0])
-    cnt = 1
+    cnt = 0
     
     for p,s in zip(progresses[1:],speeds[1:]) :
         d = math.ceil((100 - p)/s)
         if max_d < d : 
             answer.append(cnt)
-            cnt = 1
+            cnt = 0
             max_d = d
-        else :
-            cnt += 1
+        cnt += 1
     answer.append(cnt)
     
     return answer
