9f3a163750fcc6a02d38bfe515d331272a186808
chois <61316086+wowchois@users.noreply.github.com>
2022-11-26 22:54:03 +0900

    Update summer_winter_coding_2019_sql.sql

--- Algorithm/Programmers/sql/summer_winter_coding_2019_sql.sql
+++ Algorithm/Programmers/sql/summer_winter_coding_2019_sql.sql
@@ -6,3 +6,19 @@ SELECT CART_ID FROM (
 	WHERE NAME = '우유' OR NAME = '요거트'
 	GROUP BY CART_ID HAVING NAMES LIKE '%우유%' AND NAMES LIKE '%요거트%'
 ) AS MILK_YOGERT
+
+
+-- UPDATE
+-- https://school.programmers.co.kr/learn/courses/30/lessons/62284
+
+SELECT CART_ID
+FROM (
+    SELECT 
+        CART_ID
+        , COUNT(CASE WHEN NAME = 'Milk' THEN 1 END) AS MILK_CNT
+        , COUNT(CASE WHEN NAME = 'Yogurt' THEN 1 END) AS YOGURT_CNT
+    FROM CART_PRODUCTS
+    WHERE NAME IN ('Milk','Yogurt')
+    GROUP BY CART_ID
+) AS T
+WHERE MILK_CNT > 0 AND YOGURT_CNT > 0
